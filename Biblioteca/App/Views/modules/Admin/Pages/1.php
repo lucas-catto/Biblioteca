c154af399d92d07b7009ad6abad26f5e06de3ba3
lucas-catto <lucascattosilva2018@gmail.com>
2023-11-05 01:13:58 -0300

    Feita as telas de cadastrar livros e empréstimos.

--- Biblioteca/App/Views/modules/Admin/Pages/1.php
+++ Biblioteca/App/Views/modules/Admin/Pages/1.php
@@ -1,1 +1,36 @@
-1
+<?php
+    session_start();
+
+    date_default_timezone_set('America/Sao_Paulo');
+    $dateI = date("d/m/Y");
+?>
+
+<style>
+    @import url('./Views/CSS/Admin/Pages/1.css');
+</style>
+
+<main>
+
+    <h1>Registrar Empréstimo</h1>
+    
+    <form action="#" method="post">
+        <input type="text"   name="UserName"    placeholder="Nome Aluno (a)" required autocomplete="off">
+        <input type="text"   name="BookName"    placeholder="Nome Livro"     required autocomplete="off">
+        
+        <div class="date">
+            <?php
+                echo "<p class='date-item'>Data Inicial: $dateI</p>";
+                $_SESSION['DataInicial'] = $dateI;
+            ?>
+
+            <input type="date" name="dateFinal" class='date-item' min="<?php $dateI; ?>">
+        </div>
+        
+        <button type="submit">Registrar</button>
+    </form>
+</main>
+
+<!-- // verificar se o usuário existe
+// verificar se o livro existe
+// verificar a qunatidade do Livro
+// ok -->
--- Biblioteca/App/Views/modules/Admin/Pages/3.php
+++ Biblioteca/App/Views/modules/Admin/Pages/3.php
@@ -1,1 +1,17 @@
-3
+
+<style>
+    @import url('./Views/CSS/Admin/Pages/3.css');
+</style>
+
+<main>
+
+    <h1>Novo Livro</h1>
+    
+    <form action="#" method="post">
+        <input type="text"   name="titulo"     placeholder="Título"             required autocomplete="off">
+        <input type="text"   name="autor"      placeholder="Autor"              required autocomplete="off">
+        <input type="number" name="quantidade" placeholder="Quantidade" min="1" required autocomplete="off">
+        <input type="number" name="codigo"     placeholder="Código"             autocomplete="off">
+        <button type="submit">Cadastrar</button>
+    </form>
+</main>
